c86148915a9124266b2e39514a83e3fccf9a1950
jjperezaguinaga <me@jjperezaguinaga.com>
2020-10-17 16:47:01 +0000

    Integrated first demo

--- src/App.js
+++ src/App.js
@@ -11,9 +11,6 @@ import AOS from 'aos';
 import { focusHandling } from 'cruip-js-toolkit';
 
 import Home from './pages/Home';
-import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
-import ResetPassword from './pages/ResetPassword';
 
 function App() {
 
@@ -40,15 +37,6 @@ function App() {
       <Switch>
         <Route exact path="/">
           <Home />
-        </Route>
-        <Route path="/signin">
-          <SignIn />
-        </Route>
-        <Route path="/signup">
-          <SignUp />
-        </Route>
-        <Route path="/reset-password">
-          <ResetPassword />
         </Route>
       </Switch>
     </>
