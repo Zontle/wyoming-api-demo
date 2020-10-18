30dea4326b9d9bd6cc2fac38c1f98fd02626127b
jjperezaguinaga <me@jjperezaguinaga.com>
2020-10-18 20:10:00 +0000

    Adding video link

--- src/partials/HeroHome.js
+++ src/partials/HeroHome.js
@@ -58,14 +58,14 @@ function HeroHome() {
                   <path d="M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10zm0 2C5.373 24 0 18.627 0 12S5.373 0 12 0s12 5.373 12 12-5.373 12-12 12z" />
                   <path d="M10 17l6-5-6-5z" />
                 </svg>
-                <span className="ml-3">Watch the full video (2 min)</span>
+                <span className="ml-3">Watch the full video (4 min)</span>
               </button>
             </div>
 
             {/* Modal */}
             <Modal id="modal" ariaLabel="modal-headline" show={videoModalOpen} handleClose={() => setVideoModalOpen(false)}>
               <div className="relative pb-9/16">
-                <iframe className="absolute w-full h-full" src="https://player.vimeo.com/video/174002812" title="Video" allowFullScreen></iframe>
+                <iframe className="absolute w-full h-full" src="https://player.vimeo.com/video/469548586" title="SOS API" allowFullScreen></iframe>
               </div>
             </Modal>
 
